dd119b7594c5f49dd026d118a9de33473940ed25
chaerinkim1011 <joychaerin@gmail.com>
2026-05-23 10:31:57 +0900

    ADD | 플레이어 이동 보완 및 점프 구현

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,15 +3,42 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rigid;
+
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+        }
+
+        //Stop Speed
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.linearVelocity = new Vector2(rigid.linearVelocity.normalized.x * 0.5f, rigid.linearVelocity.y);
+        }
+
+        //Direction Sprite
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+        }
     }
 
     private void FixedUpdate()
     {
+
+
         //Move By Key Control
         float h = Input.GetAxisRaw("Horizontal");
 
