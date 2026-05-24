52e1001957c997023082c0a89da01ea8e8c88ccf
chaerinkim1011 <joychaerin@gmail.com>
2026-05-24 20:00:46 +0900

    공격이랑 적 움직임 수정함

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMove : MonoBehaviour
 {
+    public int hp = 100;
 
     Rigidbody2D rigid;
     SpriteRenderer spriteRenderer;
@@ -17,11 +16,14 @@ public class EnemyMove : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-       
+
     }
 
     void Update()
     {
+        if (Player == null || rigid == null || spriteRenderer == null || anim == null)
+            return;
+
         if (Player.position.x > transform.position.x)
         {
             nextMove = 1;
@@ -42,16 +44,30 @@ public class EnemyMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (rigid == null)
+            return;
+
         //Move
-     rigid.linearVelocity = new Vector2(nextMove * moveSpeed, rigid.linearVelocity.y);
+        rigid.linearVelocity = new Vector2(nextMove * moveSpeed, rigid.linearVelocity.y);
 
 
     }
+
+    public void SetDamage(int damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0)
+        {
+            hp = 0;
+            Destroy(gameObject);
+        }
+    }
 }
 
- 
 
-    //재귀함수: 자신을 스스로 호출하는 함수
-    //딜레이 없이 사용하는 것은 아주 위험하다
-    //그러므로 주어진 시간 지나고 지정된 함수를 실행하는 Invoke 사용
-   
+
+//재귀함수: 자신을 스스로 호출하는 함수
+//딜레이 없이 사용하는 것은 아주 위험하다
+//그러므로 주어진 시간 지나고 지정된 함수를 실행하는 Invoke 사용
+
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,11 +2,21 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public int hp = 100;
+    public int mp = 100;
     public float maxSpeed;
+    public int damage = 10;
+    public float minX = -8.5f;
+    public float maxX = 48.8f;
+
     SpriteRenderer spriteRenderer;
     Rigidbody2D rigid;
     Animator anim;
+    EnemyMove enemy;
+    bool isAttacking;
+    float attackTimer;
 
+    const float attackTime = 0.08f;
 
     void Awake()
     {
@@ -17,42 +27,70 @@ public class PlayerMove : MonoBehaviour
 
     void Update()
     {
-
-        //Attack Animation
-        if(Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-
-        }
-        //Stop Speed
-        if (Input.GetButtonUp("Horizontal"))
-        {
-            rigid.linearVelocity = new Vector2(rigid.linearVelocity.normalized.x * 0.5f, rigid.linearVelocity.y);
-        }
-
-        //Direction Sprite
-        if(Input.GetButtonDown("Horizontal"))
-        spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
-
-        //Animation
-        if (Mathf.Abs(rigid.linearVelocity.x) < 0.3)
+            isAttacking = true;
+            attackTimer = attackTime;
             anim.SetBool("isRunning", false);
-        else
-            anim.SetBool("isRunning", true);
+            anim.SetBool("isAttacking", true);
+            anim.Play("Player_Attack", 0, 0f);
+
+            if (enemy != null)
+                enemy.SetDamage(damage);
+        }
+
+        if (isAttacking)
+        {
+            attackTimer -= Time.deltaTime;
+
+            if (attackTimer <= 0)
+            {
+                isAttacking = false;
+                anim.SetBool("isAttacking", false);
+            }
+        }
+
+        if (Input.GetButtonUp("Horizontal"))
+            rigid.linearVelocity = new Vector2(rigid.linearVelocity.normalized.x * 0.5f, rigid.linearVelocity.y);
+
+        if (Input.GetButtonDown("Horizontal"))
+            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") == -1;
+
+        if (!isAttacking)
+            anim.SetBool("isRunning", Mathf.Abs(rigid.linearVelocity.x) >= 0.3f);
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-
-
-        //Move By Key Control
         float h = Input.GetAxisRaw("Horizontal");
-
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
 
         if (rigid.linearVelocity.x > maxSpeed)
             rigid.linearVelocity = new Vector2(maxSpeed, rigid.linearVelocity.y);
-        else if (rigid.linearVelocity.x < maxSpeed*(-1))
-            rigid.linearVelocity = new Vector2(maxSpeed * (-1), rigid.linearVelocity.y);
 
+        if (rigid.linearVelocity.x < -maxSpeed)
+            rigid.linearVelocity = new Vector2(-maxSpeed, rigid.linearVelocity.y);
+
+        float x = Mathf.Clamp(transform.position.x, minX, maxX);
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+
+    public void SetDamage(int damage)
+    {
+        hp -= damage;
+
+        if (hp <= 0)
+            hp = 0;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        enemy = collision.gameObject.GetComponent<EnemyMove>();
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<EnemyMove>() == enemy)
+            enemy = null;
     }
 }
